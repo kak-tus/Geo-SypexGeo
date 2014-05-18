9f086cbf4d49fecdaac61bc2978c641bd4c882f1
Kaktus <kak-tus@mail.ru>
2014-05-18 21:58:16 +0400

    Добавлен пример.

---
+++ example/ex.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use v5.10;
+
+use lib 'lib';
+
+use Geo::SypexGeo;
+
+binmode STDOUT, ':utf8';
+
+my $geo = Geo::SypexGeo->new( 'data/SxGeoCity.dat' );
+
+my $city;
+
+$city = $geo->get_city( '87.250.250.203' );
+say $city;
+
+$city = $geo->get_city( '93.191.14.81' );
+say $city;
+
